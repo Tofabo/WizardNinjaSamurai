6539c4632902dd8bc51ffc2a2b1967bb72689f87
allieneko <betsie@gmail.com>
2017-01-12 17:06:22 -0800

    starting terminalrpg

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Threading;
+using System.Collections.Generic;
 
-namespace ConsoleApplication
+namespace scary
 {
     public class Human
 {
@@ -48,7 +49,8 @@ namespace ConsoleApplication
     }
 
     public new string ToString(){
-        return "Name: " + name +", strength: " + strength + ", intelligence: " + intelligence + ", dexterity: " + dexterity + ", health: "+ health;
+        // return "Name: " + name +", strength: " + strength + ", intelligence: " + intelligence + ", dexterity: " + dexterity + ", health: "+ health;
+        return name + " is at " + health + " health!";
     }
 }
 
@@ -65,7 +67,7 @@ public class Wizard : Human{
     }
 
     public void fireball(object victim){
-        Human enemy = victim as Human; //does this work or does it reset some of the default values??
+        Enemy enemy = victim as Enemy; //does this work or does it reset some of the default values??
         Random rnd = new Random();
         int damage = rnd.Next(20,51);
         enemy.health -= damage; //check if object has health??? something
@@ -78,7 +80,7 @@ public class Ninja : Human{
     }
 
     public void steal(object victim){
-        Human enemy = victim as Human;
+        Enemy enemy = victim as Enemy;
         enemy.health -= 10;
         health += 10;
     }
@@ -98,7 +100,7 @@ public class Samurai : Human{
 
 
     public void death_blow(object victim){
-        Human enemy = victim as Human;
+        Enemy enemy = victim as Enemy;
         if(enemy.health < 50){
             enemy.health = 0;
         }
@@ -120,29 +122,122 @@ public class Samurai : Human{
     {
         public static void Main(string[] args)
         {
-            Human qt = new Human("Amy");
+
             Wizard harry = new Wizard("Harry");
             Ninja liz = new Ninja("Liz");
-            Samurai jack = new Samurai("Jack");
-            Samurai bill = new Samurai("Bill");
             Samurai allie = new Samurai("Allie");
-            Samurai seth = new Samurai("Seth");
-            // System.Console.WriteLine(qt.ToString());
-            // System.Console.WriteLine(harry.ToString());
-            System.Console.WriteLine(jack.ToString());
-            // System.Console.WriteLine(jack.ToString());
 
-            harry.fireball(jack);
-            System.Console.WriteLine(jack.ToString());
-            jack.death_blow(jack);
-            jack.meditate();
-            jack.how_many();
-            // System.Console.WriteLine(Samurai.instanceCount);
-            // liz.get_away();
-            // System.Console.WriteLine(harry.ToString());
-            System.Console.WriteLine(jack.ToString());
-            // System.Console.WriteLine(harry.ToString());
+            List<Human> Party = new List<Human>();
+                Party.Add(allie);
+                Party.Add(liz);
+                Party.Add(harry);
+
+            Spider nightmare = new Spider("Nightmare");
+            Zombie billy = new Zombie("Billy");
+            Zombie todd = new Zombie("Todd");
             
-        }
+            List<Enemy> Enemies = new List<Enemy>();
+                Enemies.Add(nightmare);
+                Enemies.Add(billy);
+                Enemies.Add(todd);
+
+                Random rnd = new Random();
+
+
+
+            // System.Console.WriteLine(nightmare.ToString());
+            // System.Console.WriteLine(billy.ToString());
+
+            System.Console.WriteLine("Your party has encountered some enemies!");
+            for (var i=0; i < Enemies.Count; i++) {
+                    System.Console.WriteLine("\t" + Enemies[i].name + ", a " + Enemies[i].GetType().Name + ", is at " + Enemies[i].health + "!");
+                }
+
+                System.Console.WriteLine("You're screwed! \nWho would you like to attack?");
+                object targetInput = Console.ReadLine();
+                Enemy partyVictim = targetInput as Enemy;
+
+            System.Console.WriteLine("What do you want to do?");
+            System.Console.WriteLine("\tLiz can steal and get_away,\n\tAllie can death_blow and meditate, and \n\tHarry can heal himself (sorry, he's not a team player) and fireball the enemy!");
+            string InputLine = Console.ReadLine();
+
+
+                // for (var i=0; i < Enemies.Count; i++) {
+                //     System.Console.WriteLine(Enemies[i].name + ", a " + Enemies[i].GetType().Name + ", is at " + Enemies[i].health + "!");
+                // }
+
+                // System.Console.WriteLine("What would you like to attack?");
+                // object targetInput = Console.ReadLine();
+
+
+                System.Console.WriteLine("********************************");
+
+            while (partyVictim.health > 0) {
+
+                int target = rnd.Next(Party.Count);
+                Human enemy_target = Party[target];
+            
+                if (InputLine == "steal") {
+                    liz.steal(partyVictim);
+                }
+                else if (InputLine == "get_away") {
+                    liz.get_away();
+                }
+                else if (InputLine == "death_blow") {
+                    allie.death_blow(partyVictim);
+                }
+
+                else if (InputLine == "meditate") {
+                    allie.meditate();
+                }
+
+                else if (InputLine == "heal") {
+                    harry.heal();
+                }
+
+                else if (InputLine == "fireball") {
+                    harry.fireball(partyVictim);
+                }
+                else {
+                    System.Console.WriteLine("You should probably do something, you know, if you wanna live...");
+                }
+                
+
+                if (partyVictim.health <= 0) {
+                    System.Console.WriteLine("You are victorious against the " + partyVictim.name + "! But there are totally more things you need to kill...");
+                    Enemies.Remove(partyVictim);
+                    break;
+                }
+
+                System.Console.WriteLine(Enemies[0].name + "'s turn to attack!");
+
+                System.Console.WriteLine(enemy_target.ToString());
+                billy.bite(enemy_target);
+                System.Console.WriteLine(enemy_target.name + " was bit! Ouch! Looks painful...");
+
+                if (enemy_target.health <= 0) {
+                    System.Console.WriteLine("Sucks to be you! " + enemy_target.name + " is totally dead.");
+                    break;
+                }
+
+
+
+                System.Console.WriteLine("Ninjas can steal and get_away, Samurai can death_blow and meditate, and Wizard can heal (himself, sorry, he's not a team player) and fireball the enemy!");
+                InputLine = Console.ReadLine();
+                // System.Console.WriteLine(InputLine);
+
+
+
+            }
+
+            // System.Console.WriteLine(nightmare.ToString());
+            
+            
+            
+            
+            }
+            
+        
     }
 }
+
